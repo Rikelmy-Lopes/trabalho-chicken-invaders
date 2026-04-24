4490fa192df6bd2c8d5c71f0f08cf5ee47e3058f
Rikelmy Lopes <rikelmylopes899@gmail.com>
2026-04-24 16:23:10 -0300

    melhorias na movimentação do jogador

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
 import pygame
 
-largura, altura = 800, 600
-positionX, positionY = 350, 250
-speed = 2
+largura = 800
+altura = 600
+speed = 5
 fps = 60
+
+player = {
+    "positionX": 0.0, 
+    "positionY": 0.0,
+    "width": 100,
+    "height": 100,
+    "color": (255, 0, 0)
+}
+
+player["positionX"] = (largura - player["width"]) / 2
+player["positionY"] = altura - player["height"]
 
 pygame.init()
 
@@ -13,31 +24,49 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Chicken Invaders")
 
 
+def move_player(player, keys, speed):
+    # limita a area jogavel
+    if keys[pygame.K_LEFT] and player["positionX"] > 0:
+        player["positionX"] -= speed
+
+    if keys[pygame.K_RIGHT] and (player["positionX"] + player["width"]) < largura:
+        player["positionX"] += speed
+
+    if keys[pygame.K_UP] and player["positionY"] > 0:
+        player["positionY"] -= speed
+
+    if keys[pygame.K_DOWN] and (player["positionY"] + player["height"] < altura):
+        player["positionY"] += speed
+
+    # reseta a posicao do player caso ele ultrapasse os limites
+    if (player["positionX"] < 0):
+        player["positionX"] = 0
+    elif player["positionX"] + player["width"] > largura:
+        player["positionX"] = (largura - player["width"])
+
+
 rodando = True
 while rodando:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             rodando = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_F5:
+                print("Aumentando velocidade")
+                speed += 2
             
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
-        positionX -= speed
-    if keys[pygame.K_RIGHT]:
-        positionX += speed
-    if keys[pygame.K_UP]:
-        positionY -= speed
-    if keys[pygame.K_DOWN]:
-        positionY += speed
-
-
+    move_player(player, keys, speed)
 
     tela.fill((30, 30, 30))
 
-    pygame.draw.rect(tela, (255, 0, 0), (positionX, positionY, 100, 100))
+    pygame.draw.rect(tela, player["color"], (player["positionX"], player["positionY"], player["width"], player["height"]))
 
     pygame.display.flip()
     clock.tick(fps)
 
 pygame.quit()   
+
+
